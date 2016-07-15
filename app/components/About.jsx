4fc09af6f7d185fc354cb2e703967966ca5a8103
Devin Chaves <dchav90@gmail.com>
2016-07-15 09:50:05 -0500

    Styled about page + links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,32 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build in React. I built this
+        for the Complete React Developer Course taught by <a href="http://www.mead.io">Andrew Mead</a>.
+      </p>
+      <p>Here are some of the tools I used:</p>
+      <ol>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - The
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org/">OpenWeatherMap</a> - The weather API
+            called upon.
+        </li>
+        <li>
+          <a href="https://webpack.github.io/">Webpack</a> - The module loader
+            used.
+        </li>
+        <li>
+          <a href="https://github.com/mzabriskie/axios">Axios</a> - The HTTP request
+            client used.
+        </li>
+      </ol>
+      <p>Along the way I encountered some problems I had
+        to reason about. Here are my thoughts:
+      </p>
     </div>
   )
 };
